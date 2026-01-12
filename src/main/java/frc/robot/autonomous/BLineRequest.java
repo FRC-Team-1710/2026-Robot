71a9af559bff95665f1a7347d853794a048e2cfb
CARTeR <carterc.eilert@gmail.com>
2026-01-12 17:10:44 -0600

    Refactor autonomous commands: replace AutoCommands with AutoBuilder, implement BLineRequest, and remove LinearPathRequest and AutoRoutines

---
+++ src/main/java/frc/robot/autonomous/BLineRequest.java
@@ -0,0 +1,39 @@
+package frc.robot.autonomous;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.swerve.SwerveDrivetrain.SwerveControlParameters;
+import com.ctre.phoenix6.swerve.SwerveModule;
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveModule.SteerRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import edu.wpi.first.math.geometry.Pose2d;
+
+public class BLineRequest implements SwerveRequest {
+  public Pose2d TargetPose = Pose2d.kZero;
+
+  private final SwerveRequest.ApplyFieldSpeeds driveRequest =
+      new SwerveRequest.ApplyFieldSpeeds()
+          .withDriveRequestType(DriveRequestType.Velocity)
+          .withSteerRequestType(SteerRequestType.MotionMagicExpo);
+
+  public BLineRequest() {}
+
+  @Override
+  public StatusCode apply(
+      SwerveControlParameters parameters, SwerveModule<?, ?, ?>... modulesToApply) {
+
+    //TODO: Implement BLine path following logic here
+
+    return driveRequest.apply(parameters, modulesToApply);
+  }
+
+  public boolean isFinished() {
+    //TODO: Implement ending logic here
+    return false;
+  }
+
+  public BLineRequest withTargetPose(Pose2d targetPose) {
+    this.TargetPose = targetPose;
+    return this;
+  }
+}
