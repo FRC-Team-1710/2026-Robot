61ba79dd67675518027ba714aec364e13d483ae8
Alex <alex.m.diehl1@gmail.com>
2026-01-16 17:49:08 -0600

    Added Shooter to RobotContainer

--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -1,9 +1,12 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.units.measure.AngularVelocity;
 
 public interface ShooterIO {
-
   public default void stop() {}
   public default void setVelocity(AngularVelocity velocity) {}
 }
