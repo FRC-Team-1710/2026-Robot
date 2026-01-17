9764fc2b798ea5550322fbcc4c7b9a73f54231de
Conley Becker <conleytobias@outlook.com>
2026-01-17 09:27:59 -0600

    Add Visual Sim

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSIM.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSIM.java
@@ -4,13 +4,19 @@
 
 package frc.robot.subsystems.intake;
 import edu.wpi.first.epilogue.Logged;
+import frc.robot.utils.MechanismUtil.ArmMechanism;
 
 @Logged
 public class IntakeIOSIM implements IntakeIO {
   /** Creates a new IntakeIOSIM. */
-  public IntakeIOSIM() {}
+  ArmMechanism armMechanism;
+
+  public IntakeIOSIM() {
+    armMechanism = new ArmMechanism("Intake", 100); //creates the visual sim
+  }
 
   public void periodic() {
     // This method will be called once per scheduler run
+    armMechanism.update(0, false); //updates visuals
   }
 }
