971fe8a1e89961d4e8a2910012df00fa598e589f
CARTeR <carterc.eilert@gmail.com>
2026-01-17 15:34:22 -0600

    Refactor package naming for Index subsystem: change package name from 'Index' to 'index' for consistency

--- src/main/java/frc/robot/subsystems/Index/IndexerIO.java
+++ src/main/java/frc/robot/subsystems/Index/IndexerIO.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.Index;
+package frc.robot.subsystems.index;
 
 public interface IndexerIO {
   public default void setIndexMotor(double speed) {}
