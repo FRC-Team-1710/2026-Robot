e0696d726d322a81f82c81a7475811be6e674984
Alex <alex.m.diehl1@gmail.com>
2026-01-19 14:47:31 -0600

    Flywheel Config

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOCTRE.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOCTRE.java
@@ -1,12 +1,18 @@
 package frc.robot.subsystems.shooter;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import frc.robot.constants.ShooterConstants;
 import frc.robot.generated.TunerConstants;
+import frc.robot.utils.TalonFXUtil;
+import frc.robot.Robot;
 
 @Logged
 public class ShooterIOCTRE implements ShooterIO {
@@ -20,6 +26,26 @@ public class ShooterIOCTRE implements ShooterIO {
     this.m_flyWheel = new TalonFX(ShooterConstants.FLYWHEEL_MOTOR_ID, TunerConstants.kCANBus);
     this.m_hood = new TalonFX(ShooterConstants.HOOD_MOTOR_ID, TunerConstants.kCANBus);
 
+    // Flywheel settings
+    TalonFXConfiguration m_config = new TalonFXConfiguration();
+    
+    m_config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    m_config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+
+    m_config.Slot0.kS = ShooterConstants.kS; // Static friction
+    m_config.Slot0.kV = ShooterConstants.kV; // Velocity feedforward
+    m_config.Slot0.kP = ShooterConstants.kP; // Proportional gain
+
+    m_config.MotionMagic.MotionMagicCruiseVelocity = ShooterConstants.MOTION_MAGIC_CRUISE_VELOCITY;
+    m_config.MotionMagic.MotionMagicAcceleration = ShooterConstants.MOTION_MAGIC_ACCELERATION;
+
+    if (TalonFXUtil.applyConfigWithRetries(this.m_flyWheel, m_config, 2)) {
+      Robot.telemetry().log("Flywheel/Config", true);
+    } else {
+      Robot.telemetry().log("Flywheel/Config", false);
+    }
+
+    
     this.m_velocityManager = new MotionMagicVelocityVoltage(0);
   }
 
