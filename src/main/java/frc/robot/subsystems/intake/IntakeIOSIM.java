baaccb769208fac2a45a99b504d3ff82a3cd6744
Conley Becker <conleytobias@outlook.com>
2026-01-19 18:02:38 -0600

    Refactor IntakeIOSIM to integrate physics simulation and visual updates

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSIM.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSIM.java
@@ -3,20 +3,30 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems.intake;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Radians;
+
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import frc.robot.utils.MechanismUtil.ArmMechanism;
 
 @Logged
 public class IntakeIOSIM implements IntakeIO {
   /** Creates a new IntakeIOSIM. */
-  ArmMechanism armMechanism;
+  private final SingleJointedArmSim armPhysicsSim;
+  private final ArmMechanism armVisualSim;
+  private Angle angleSetpoint;
 
   public IntakeIOSIM() {
-    armMechanism = new ArmMechanism("Intake", 100); //creates the visual sim
+    armPhysicsSim = new SingleJointedArmSim(null, 1, 0, 0, 0, 0, true, 0, null);
+    armVisualSim = new ArmMechanism("Intake", 100); //creates the visual sim
   }
-
-  public void periodic() {
-    // This method will be called once per scheduler run
-    armMechanism.update(0, false); //updates visuals
+  
+  public void setAngle(Angle angle) {
+    angleSetpoint = angle;
+    armPhysicsSim.setState(angle.magnitude(), Units.degreesToRadians(360));
+    armVisualSim.update(Units.radiansToDegrees(armPhysicsSim.getAngleRads()), Math.abs(angleSetpoint.in(Radians) - armPhysicsSim.getAngleRads()) < Units.degreesToRadians(1) ); //updates visuals
   }
 }
