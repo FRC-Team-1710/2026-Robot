f9f24072e2a089749bd50b8a1191a7bcb7d5ac2f
RavenRain44 <89553123+RavenRain44@users.noreply.github.com>
2026-01-20 20:23:04 -0600

    Add ShooterMath utility class for shooter-related calculations

---
+++ src/main/java/frc/robot/utils/ShooterMath/ShooterMath.java
@@ -0,0 +1,8 @@
+package frc.robot.utils.ShooterMath;
+
+/** Utility class for shooter-related mathematical calculations. */
+public class ShooterMath {
+  private static final double MASS_FUEL = 0.21; // IN KG
+
+  
+}
