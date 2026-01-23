b7af6135a9b61360de4b475614404d40e1850d63
Conley Becker <conleytobias@outlook.com>
2026-01-22 18:56:52 -0600

    Updated sim & spotless formatting

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSIM.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSIM.java
@@ -3,7 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems.intake;
-import static edu.wpi.first.units.Units.Degrees;
+
 import static edu.wpi.first.units.Units.Radians;
 
 import edu.wpi.first.epilogue.Logged;
@@ -17,19 +17,24 @@ import frc.robot.utils.MechanismUtil.ArmMechanism;
 public class IntakeIOSIM implements IntakeIO {
   /** Creates a new IntakeIOSIM. */
   private final DCMotor gearbox;
+
   private final SingleJointedArmSim armPhysicsSim;
   private final ArmMechanism armVisualSim;
   private Angle angleSetpoint;
 
   public IntakeIOSIM() {
     gearbox = DCMotor.getKrakenX60(1);
-    armPhysicsSim = new SingleJointedArmSim(gearbox, 25, 0.004, 0.1, -45, 90, true, 0, null);
-    armVisualSim = new ArmMechanism("Intake", 100); //creates the visual sim
+    armPhysicsSim =
+        new SingleJointedArmSim(gearbox, 25, 0.004, 0.1, -45, 90, true, 0, new double[2]);
+    armVisualSim = new ArmMechanism("Intake", 100); // creates the visual sim
   }
-  
+
   public void setAngle(Angle angle) {
     angleSetpoint = angle;
     armPhysicsSim.setState(angle.magnitude(), Units.degreesToRadians(360));
-    armVisualSim.update(Units.radiansToDegrees(armPhysicsSim.getAngleRads()), Math.abs(angleSetpoint.in(Radians) - armPhysicsSim.getAngleRads()) < Units.degreesToRadians(1) ); //updates visuals
+    armVisualSim.update(
+        Units.radiansToDegrees(armPhysicsSim.getAngleRads()),
+        Math.abs(angleSetpoint.in(Radians) - armPhysicsSim.getAngleRads())
+            < Units.degreesToRadians(1)); // updates visuals
   }
 }
