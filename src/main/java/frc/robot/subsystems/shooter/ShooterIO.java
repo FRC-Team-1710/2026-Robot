23be20b379db7f59872765511f72ff9d7f68413d
Alex <alex.m.diehl1@gmail.com>
2026-01-22 19:01:14 -0600

    Added update function to ShooterIO

--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -8,9 +8,19 @@ import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 
 public interface ShooterIO {
+  public default void update() {}
+
   public default void stop() {}
+
   public default void setVelocity(AngularVelocity velocity) {}
 
+  public default AngularVelocity getVelocity() {
+    return null;
+  }
+
   public default void setHoodAngle(Angle angle) {}
-  public default Angle getHoodAngle() { return null; }
+
+  public default Angle getHoodAngle() {
+    return null;
+  }
 }
