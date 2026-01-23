a55b7e6d256f90c5481f77af1a0ff8e943ff8a79
Alex <alex.m.diehl1@gmail.com>
2026-01-23 15:56:09 -0600

    @Logged ShooterIO

--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -4,9 +4,11 @@
 
 package frc.robot.subsystems.shooter;
 
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 
+@Logged
 public interface ShooterIO {
   public default void update() {}
 
