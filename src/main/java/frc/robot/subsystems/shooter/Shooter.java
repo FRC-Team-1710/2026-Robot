705fb5a6bfdc0bbf478c4311c32ea045ba200a6d
Alex <alex.m.diehl1@gmail.com>
2026-01-22 18:59:57 -0600

    Added Hood angle to SHOOTER_STATE

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems.shooter;
 
+import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -10,19 +12,20 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Shooter extends SubsystemBase {
 
   public enum SHOOTER_STATE {
-    STOP(RotationsPerSecond.of(0)),
+    STOP(RotationsPerSecond.of(0), Degrees.of(0)),
 
-    IDLE(RotationsPerSecond.of(200)),
-    SHOOT(RotationsPerSecond.of(250)),
+    IDLE(RotationsPerSecond.of(200), Degrees.of(0)),
+    SHOOT(RotationsPerSecond.of(250), Degrees.of(0)),
 
-    PRESET_PASS(RotationsPerSecond.of(100)),
-    PRESET_SHOOT(RotationsPerSecond.of(250));
-
+    PRESET_PASS(RotationsPerSecond.of(100), Degrees.of(0)),
+    PRESET_SHOOT(RotationsPerSecond.of(250), Degrees.of(0));
 
     private final AngularVelocity m_velocity;
+    private final Angle m_hoodAngle;
 
-    SHOOTER_STATE(AngularVelocity velocity) {
+    SHOOTER_STATE(AngularVelocity velocity, Angle hoodAngle) {
       this.m_velocity = velocity;
+      this.m_hoodAngle = hoodAngle;
     }
   };
 
@@ -30,39 +33,38 @@ public class Shooter extends SubsystemBase {
 
   private final ShooterIO m_io;
 
-
   public Shooter(ShooterIO io) {
+
     this.m_io = io;
     this.m_state = SHOOTER_STATE.STOP;
   }
 
   @Override
   public void periodic() {
+    this.m_io.setHoodAngle(m_state.m_hoodAngle);
+
     switch (this.m_state) {
       case STOP:
         this.m_io.stop();
         break;
 
-
-      case IDLE:
-        this.m_io.setVelocity(SHOOTER_STATE.IDLE.m_velocity);
-        break;
-
-      case SHOOT:
-        this.m_io.setVelocity(SHOOTER_STATE.SHOOT.m_velocity);
-        break;
-
-
-        case PRESET_PASS:
-        this.m_io.setVelocity(SHOOTER_STATE.PRESET_PASS.m_velocity);
-        break;
-        
-      case PRESET_SHOOT:
-        this.m_io.setVelocity(SHOOTER_STATE.PRESET_SHOOT.m_velocity);
+      default:
+        this.m_io.setVelocity(m_state.m_velocity);
         break;
     }
+
+    this.m_io.update();
   }
 
+  public void setVelocity(AngularVelocity velocity) {}
+
+  public void setHoodAngle(Angle angle) {
+    this.m_io.setHoodAngle(angle);
+  }
+
+  public Angle getHoodAngle() {
+    return this.m_io.getHoodAngle();
+  }
 
   public void setState(SHOOTER_STATE state) {
     this.m_state = state;
