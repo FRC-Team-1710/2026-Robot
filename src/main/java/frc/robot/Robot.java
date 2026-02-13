7fd11ff61d387956f6ccb7aac325e7c95caa2c03
CARTeR <carterc.eilert@gmail.com>
2026-02-12 19:38:25 -0600

    add dynamic back to robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -21,7 +21,6 @@ import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PowerDistribution;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -36,7 +35,7 @@ import java.util.HashMap;
 
 @Logged
 @SuppressWarnings("unused")
-public class Robot extends TimedRobot {
+public class Robot extends DynamicTimedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
