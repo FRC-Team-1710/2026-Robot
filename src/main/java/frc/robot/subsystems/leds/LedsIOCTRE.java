c1eff961b1835c1418c5346d3b48365af61a6649
AlexIsCool321 <alex.m.diehl1@gmail.com>
2026-02-17 19:10:45 -0600

    send message

--- src/main/java/frc/robot/subsystems/leds/LedsIOCTRE.java
+++ src/main/java/frc/robot/subsystems/leds/LedsIOCTRE.java
@@ -2,20 +2,38 @@ package frc.robot.subsystems.leds;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 @Logged
 public class LedsIOCTRE implements LedsIO {
-  private final DigitalOutput JohnRomero; // Bit 1 (1)
-  private final DigitalOutput JohnCarmack; // Bit 2 (2)
-  private final DigitalOutput AdrianCarmack; // Bit 3 (4)
-  private final DigitalOutput SandyPetersen; // Bit 4 (8)
+  private boolean[] m_output;
+
+  private final DigitalOutput m_johnRomero; // Bit 1 (1)
+  private final DigitalOutput m_johnCarmack; // Bit 2 (2)
+  private final DigitalOutput m_adrianCarmack; // Bit 3 (4)
+  private final DigitalOutput m_sandyPetersen; // Bit 4 (8)
+
+  public final DigitalOutput[] m_bits;
 
   public LedsIOCTRE() {
-    this.JohnRomero = new DigitalOutput(3);
-    this.JohnCarmack = new DigitalOutput(4);
-    this.AdrianCarmack = new DigitalOutput(5);
-    this.SandyPetersen = new DigitalOutput(6);
+    this.m_output = new boolean[4];
+
+    this.m_johnRomero = new DigitalOutput(3);
+    this.m_johnCarmack = new DigitalOutput(4);
+    this.m_adrianCarmack = new DigitalOutput(5);
+    this.m_sandyPetersen = new DigitalOutput(6);
+
+    this.m_bits =
+        new DigitalOutput[] {m_johnRomero, m_johnCarmack, m_adrianCarmack, m_sandyPetersen};
   }
 
   public void update(double pDTSeconds) {}
+
+  public void send() {
+    for (int i = 0; i < this.m_output.length; i++) {
+      this.m_bits[i].set(this.m_output[i]);
+    }
+
+    SmartDashboard.putBooleanArray("DigitalOutputs", this.m_output);
+  }
 }
