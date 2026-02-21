f3a229b217d011e2bea356c1600d1e9ff1c094d0
AlexIsCool321 <alex.m.diehl1@gmail.com>
2026-02-21 10:38:08 -0600

    bit minpulation

---
+++ src/main/java/frc/robot/constants/GoodExampleConstants.java
@@ -0,0 +1,5 @@
+package frc.robot.constants;
+
+public class GoodExampleConstants {
+  public static int MAX_BALL_COUNT = 2;
+}
--- src/main/java/frc/robot/subsystems/leds/LedsIO.java
+++ src/main/java/frc/robot/subsystems/leds/LedsIO.java
@@ -6,5 +6,15 @@ import edu.wpi.first.epilogue.Logged;
 public interface LedsIO {
   public default void update(double pDTSeconds) {}
 
+  public enum LED_STATE {
+    AUTOS,
+    BROWOUT,
+    ATTACKING
+  };
+
+  public default void setValue(LED_STATE pState, boolean pValue) {}
+
+  public default void setFlyWheelCharge(int pPercentage) {}
+
   public default void send() {}
 }
