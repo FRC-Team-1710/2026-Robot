42de6389d7de037d4d70bba129d8ff6e7f0b6ce0
AlexIsCool321 <alex.m.diehl1@gmail.com>
2026-02-21 11:10:52 -0600

    superstructure implemtation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -38,6 +38,10 @@ import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.IntakeIO;
 import frc.robot.subsystems.intake.IntakeIOCTRE;
 import frc.robot.subsystems.intake.IntakeIOSIM;
+import frc.robot.subsystems.leds.Leds;
+import frc.robot.subsystems.leds.LedsIO;
+import frc.robot.subsystems.leds.LedsIOCTRE;
+import frc.robot.subsystems.leds.LedsIOSIM;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.ShooterIO;
 import frc.robot.subsystems.shooter.ShooterIOCTRE;
@@ -74,6 +78,9 @@ public class RobotContainer {
   @Logged(importance = Importance.CRITICAL)
   private final Feeder feeder;
 
+  @Logged(importance = Importance.CRITICAL)
+  private final Leds leds;
+
   // Should add logging soon
   @NotLogged private Vision[] cameras;
 
@@ -91,6 +98,7 @@ public class RobotContainer {
         feeder = new Feeder(new FeederIOCTRE(), consumer);
         indexer =
             new Indexer(new IndexerIOCTRE(), consumer, () -> driver.leftBumper().getAsBoolean());
+        leds = new Leds(new LedsIOCTRE(), shooter);
 
         cameras =
             // Create a stream of Vision objects from the camera configs
@@ -113,6 +121,7 @@ public class RobotContainer {
         feeder = new Feeder(new FeederIOSIM(), consumer);
         indexer =
             new Indexer(new IndexerIOSIM(), consumer, () -> driver.leftBumper().getAsBoolean());
+        leds = new Leds(new LedsIOSIM(), shooter);
         break;
 
       default:
@@ -121,10 +130,12 @@ public class RobotContainer {
         feeder = new Feeder(new FeederIO() {}, consumer);
         indexer =
             new Indexer(new IndexerIO() {}, consumer, () -> driver.leftBumper().getAsBoolean());
+        leds = new Leds(new LedsIO() {}, shooter);
         break;
     }
 
-    superstructure = new Superstructure(driver, mech, drivetrain, intake, shooter, indexer, feeder);
+    superstructure =
+        new Superstructure(driver, mech, drivetrain, intake, shooter, indexer, feeder, leds);
 
     // Fuel Simulation
     if (Mode.currentMode == CurrentMode.SIMULATION) {
@@ -216,22 +227,28 @@ public class RobotContainer {
             Milliseconds.of((20.0 / Subsystems.values().length) * 1)));
     map.add(
         new SubsystemInfo(
+            Subsystems.Drive,
+            drivetrain::periodic,
+            Milliseconds.of(20),
+            Milliseconds.of((20.0 / Subsystems.values().length) * 2)));
+    map.add(
+        new SubsystemInfo(
             Subsystems.Intake,
             intake::periodic,
             Milliseconds.of(60),
-            Milliseconds.of((20.0 / Subsystems.values().length) * 2 + 20.0)));
+            Milliseconds.of((20.0 / Subsystems.values().length) * 3 + 20.0)));
     map.add(
         new SubsystemInfo(
             Subsystems.Shooter,
             shooter::periodic,
             Milliseconds.of(60),
-            Milliseconds.of((20.0 / Subsystems.values().length) * 3 + 40.0)));
+            Milliseconds.of((20.0 / Subsystems.values().length) * 4 + 40.0)));
     map.add(
         new SubsystemInfo(
             Subsystems.Indexer,
             indexer::periodic,
             Milliseconds.of(60),
-            Milliseconds.of((20.0 / Subsystems.values().length) * 4 + 60.0)));
+            Milliseconds.of((20.0 / Subsystems.values().length) * 5 + 60.0)));
     map.add(
         new SubsystemInfo(
             Subsystems.Feeder,
@@ -240,13 +257,10 @@ public class RobotContainer {
             Milliseconds.of((20.0 / Subsystems.values().length) * 5 + 60.0)));
     map.add(
         new SubsystemInfo(
-            Subsystems.Vision, this::cycleVision, Milliseconds.of(20), Microseconds.of(0)));
-    map.add(
-        new SubsystemInfo(
-            Subsystems.Drive,
-            drivetrain::periodic,
+            Subsystems.Leds,
+            leds::periodic,
             Milliseconds.of(20),
-            Milliseconds.of((20.0 / Subsystems.values().length) * 5)));
+            Milliseconds.of((20.0 / Subsystems.values().length) * 7)));
     return map.toArray(new SubsystemInfo[0]);
   }
 
